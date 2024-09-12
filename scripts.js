4f68d2e65e42ebc127efec633b4ab1c6e4a20816
Stefan Johner <16019080+sjohner@users.noreply.github.com>
2024-09-12 22:13:21 +0200

    Added more content and improved design

--- scripts.js
+++ scripts.js
@@ -1,4 +1,21 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+document.addEventListener('DOMContentLoaded', () => {
+    const userLang = navigator.language || navigator.userLanguage;
+    const languageSelector = document.getElementById('language-selector');
+    if (userLang.startsWith('de')) {
+        languageSelector.value = 'de';
+        changeLanguage();
+    } else {
+        languageSelector.value = 'en';
+    }
+});
+
+function changeLanguage() {
+    const selectedLang = document.getElementById('language-selector').value;
+    document.querySelectorAll('[data-lang]').forEach(element => {
+        if (element.getAttribute('data-lang') === selectedLang) {
+            element.style.display = '';
+        } else {
+            element.style.display = 'none';
+        }
+    });
 }
